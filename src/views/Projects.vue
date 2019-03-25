baaf5dbe70674541268a05801597f01f13947e5b
Eder Ramirez <eder.ramirez87@gmail.com>
2019-03-25 13:26:07 -0600

    Creating card layouts

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,10 +1,23 @@
 <template>
   <section class="projects">
     <aside class="projects__filters"></aside>
-    <div class="projects__cards"></div>
+    <div class="projects__cards">
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
+    </div>
   </section>
 </template>
 
-<script></script>
+<script>
+import ProjectCard from "@/components/ProjectCard.vue";
+export default {
+  components: {
+    ProjectCard
+  }
+};
+</script>
 
 <style lang="scss" scoped></style>
