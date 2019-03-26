fdf186f006b2e6bbc6786534a839762388166454
Eder Ramirez <eder.ramirez87@gmail.com>
2019-03-26 10:49:15 -0600

    Adding button cards

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,9 +1,32 @@
 <template>
-  <div class="card">
-    <div class="card__img"></div>
-    <div class="card__details">
-      <div class="card__title">Project Title</div>
+  <div>
+    <div class="card">
+      <div class="card__img"></div>
+      <div class="card__details">
+        <div class="card__technologies">
+          Vue, Laravel, Javascrit, SASS
+        </div>
+        <div class="card__icon-container">
+          <svg class="card__icon">
+            <use xlink:href="../assets/icons/sprite.svg#icon-github" />
+          </svg>
+          <div>Github</div>
+        </div>
+        <div class="card__icon-container">
+          <svg class="card__icon">
+            <use xlink:href="../assets/icons/sprite.svg#icon-new-tab" />
+          </svg>
+          <div>See live</div>
+        </div>
+        <div class="card__icon-container">
+          <svg class="card__icon">
+            <use xlink:href="../assets/icons/sprite.svg#icon-arrow-right2" />
+          </svg>
+          <div>Details</div>
+        </div>
+      </div>
     </div>
+    <div class="card__title">Project Title</div>
   </div>
 </template>
 
