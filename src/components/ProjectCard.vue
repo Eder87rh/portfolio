148117c3f4790f1fa529dfb70fc0bf740e854be8
Eder Ramirez <eder.ramirez87@gmail.com>
2019-03-25 18:23:28 -0600

    Progress styling cards

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="card"></div>
+  <div class="card">
+    <div class="card__img"></div>
+    <div class="card__details">
+      <div class="card__title">Project Title</div>
+    </div>
+  </div>
 </template>
 
 <script>
