b1ceb9fea54d5373a4d300b4ba19cc4d8e7b6941
Eder Ramirez <eder.ramirez87@gmail.com>
2019-03-27 11:43:03 -0600

    Seting up tech tags

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,28 +1,36 @@
 <template>
   <div>
     <div class="card">
-      <div class="card__img"></div>
-      <div class="card__details">
+      <div class="card__img">
         <div class="card__technologies">
-          Vue, Laravel, Javascrit, SASS
+          <div class="card__technology-item">Vue.js</div>
+          <div class="card__technology-item">Laravel</div>
+          <div class="card__technology-item">CSS Grid</div>
+          <div class="card__technology-item">Vue.js</div>
+          <div class="card__technology-item">Laravel</div>
+          <div class="card__technology-item">CSS Grid</div>
         </div>
-        <div class="card__icon-container">
-          <svg class="card__icon">
-            <use xlink:href="../assets/icons/sprite.svg#icon-github" />
-          </svg>
-          <div>Github</div>
-        </div>
-        <div class="card__icon-container">
-          <svg class="card__icon">
-            <use xlink:href="../assets/icons/sprite.svg#icon-new-tab" />
-          </svg>
-          <div>See live</div>
-        </div>
-        <div class="card__icon-container">
-          <svg class="card__icon">
-            <use xlink:href="../assets/icons/sprite.svg#icon-arrow-right2" />
-          </svg>
-          <div>Details</div>
+      </div>
+      <div class="card__details">
+        <div class="card__icons">
+          <div class="card__icon-container">
+            <svg class="card__icon">
+              <use xlink:href="../assets/icons/sprite.svg#icon-github" />
+            </svg>
+            <div>Github</div>
+          </div>
+          <div class="card__icon-container">
+            <svg class="card__icon">
+              <use xlink:href="../assets/icons/sprite.svg#icon-new-tab" />
+            </svg>
+            <div>Live</div>
+          </div>
+          <div class="card__icon-container">
+            <svg class="card__icon">
+              <use xlink:href="../assets/icons/sprite.svg#icon-arrow-right2" />
+            </svg>
+            <div>Details</div>
+          </div>
         </div>
       </div>
     </div>
