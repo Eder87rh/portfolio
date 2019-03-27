afe353e69b41a670bbc9f55d70592105f3562607
Eder Ramirez <eder.ramirez87@gmail.com>
2019-03-27 11:52:14 -0600

    Refactoring code

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -6,32 +6,32 @@
           <div class="card__technology-item">Vue.js</div>
           <div class="card__technology-item">Laravel</div>
           <div class="card__technology-item">CSS Grid</div>
-          <div class="card__technology-item">Vue.js</div>
+          <!-- <div class="card__technology-item">Vue.js</div>
           <div class="card__technology-item">Laravel</div>
-          <div class="card__technology-item">CSS Grid</div>
+          <div class="card__technology-item">CSS Grid</div> -->
         </div>
       </div>
       <div class="card__details">
-        <div class="card__icons">
-          <div class="card__icon-container">
-            <svg class="card__icon">
-              <use xlink:href="../assets/icons/sprite.svg#icon-github" />
-            </svg>
-            <div>Github</div>
-          </div>
-          <div class="card__icon-container">
-            <svg class="card__icon">
-              <use xlink:href="../assets/icons/sprite.svg#icon-new-tab" />
-            </svg>
-            <div>Live</div>
-          </div>
-          <div class="card__icon-container">
-            <svg class="card__icon">
-              <use xlink:href="../assets/icons/sprite.svg#icon-arrow-right2" />
-            </svg>
-            <div>Details</div>
-          </div>
+        <!-- <div class="card__icons"> -->
+        <div class="card__icon-container">
+          <svg class="card__icon">
+            <use xlink:href="../assets/icons/sprite.svg#icon-github" />
+          </svg>
+          <div>Github</div>
         </div>
+        <div class="card__icon-container">
+          <svg class="card__icon">
+            <use xlink:href="../assets/icons/sprite.svg#icon-new-tab" />
+          </svg>
+          <div>Live</div>
+        </div>
+        <div class="card__icon-container">
+          <svg class="card__icon">
+            <use xlink:href="../assets/icons/sprite.svg#icon-arrow-right2" />
+          </svg>
+          <div>Details</div>
+        </div>
+        <!-- </div> -->
       </div>
     </div>
     <div class="card__title">Project Title</div>
