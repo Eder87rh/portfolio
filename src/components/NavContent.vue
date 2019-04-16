925145ef72320747e423477b33961fe0cf0d5a0d
Eder Ramirez <eder.ramirez87@gmail.com>
2019-04-16 17:13:03 -0500

    little fixes

--- src/components/NavContent.vue
+++ src/components/NavContent.vue
@@ -3,36 +3,24 @@
     <ul class="nav-content">
       <li class="nav-content__item">
         <router-link class="nav-content__link" :to="{ name: 'projects' }">
-          <!--  <svg class="nav-content__icon">
-              <use xlink:href="../assets/icons/sprite.svg#icon-home" />
-            </svg> -->
           <span>Projects</span>
         </router-link>
       </li>
       <li class="nav-content__item">
         <router-link class="nav-content__link" to="#">
-          <!-- <svg class="nav-content__icon">
-              <use xlink:href="../assets/icons/sprite.svg#icon-github" />
-            </svg> -->
           <span>About me</span>
         </router-link>
       </li>
-      <li class="nav-content__item">
+      <!-- <li class="nav-content__item">
         <router-link class="nav-content__link" to="#">
-          <!-- <svg class="nav-header__icon">
-              <use xlink:href="../assets/icons/sprite.svg#icon-linkedin" />
-            </svg> -->
           <span>Courses</span>
         </router-link>
       </li>
       <li class="nav-content__item">
         <router-link class="nav-content__link" to="#">
-          <!-- <svg class="nav-header__icon">
-              <use xlink:href="../assets/icons/sprite.svg#icon-linkedin" />
-            </svg> -->
           <span>Certifications</span>
         </router-link>
-      </li>
+      </li> -->
     </ul>
   </nav>
 </template>
