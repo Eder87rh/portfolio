b0491f2245d5563541ebb683ada4303c31e1a3bd
Eder Ramirez <eder.ramirez87@gmail.com>
2019-04-16 17:03:01 -0500

    Improving the banner

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="header">
     <NavHeader />
-    <div class="header__text-box">
-      <h1 class="heading-primary">
-        <!--  <span class="heading-primary--main">Eder ramírez Hernández</span> -->
-        <span class="heading-primary--main">Port Community</span>
-        <!-- <span class="heading-primary--sub">Full-stack developer</span> -->
-        <span class="heading-primary--sub"
-          >Administracion Portuaria de Lázaro Cárdenas</span
-        >
-      </h1>
+    <div class="banner-texbox">
+      <div class="banner-texbox__avatar"></div>
+      <div class="banner-texbox__title">
+        <h1 class="heading-primary margin-bottom-small">
+          <!--  <span class="heading-primary--main">Eder ramírez Hernández</span> -->
+          <span class="heading-primary--main">Port Community</span>
+          <!-- <span class="heading-primary--sub">Full-stack developer</span> -->
+          <span class="heading-primary--sub"
+            >Administracion Portuaria de Lázaro Cárdenas</span
+          >
+        </h1>
+        <button class="button" href="#">Hire me</button>
+      </div>
+
       <!-- <a href="#" class="btn btn--white btn--animated">Hire me</a> -->
     </div>
     <NavContent />
