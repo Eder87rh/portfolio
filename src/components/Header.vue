589c0d8ef4e7089637945d852f20950b33e166dd
Eder Ramirez <eder.ramirez87@gmail.com>
2019-04-17 11:57:13 -0500

    Adding avatar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,15 +2,17 @@
   <div class="header">
     <NavHeader />
     <div class="banner-texbox">
-      <div class="banner-texbox__avatar"></div>
+      <figure class="banner-texbox__avatar">
+        <img src="img/me.jpg" alt="Eder Ramírez" class="banner-texbox__img" />
+      </figure>
       <div class="banner-texbox__title">
         <h1 class="heading-primary margin-bottom-small">
-          <!--  <span class="heading-primary--main">Eder ramírez Hernández</span> -->
-          <span class="heading-primary--main">Port Community</span>
-          <!-- <span class="heading-primary--sub">Full-stack developer</span> -->
-          <span class="heading-primary--sub"
-            >Administracion Portuaria de Lázaro Cárdenas</span
-          >
+          <span class="heading-primary--main">Eder Ramírez</span>
+          <!-- <span class="heading-primary--main">Port Community</span> -->
+          <span class="heading-primary--sub">Full-stack developer</span>
+          <!-- <span class="heading-primary--sub" -->
+          <!-- >Administracion Portuaria de Lázaro Cárdenas</span -->
+          <!-- > -->
         </h1>
         <button
           class="button"
