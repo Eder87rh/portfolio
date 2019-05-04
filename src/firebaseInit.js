1788a5160de42cefd8122bde6706db669d4cf8e8
Eder Ramirez <eder.ramirez87@gmail.com>
2019-05-04 12:41:17 -0500

    Changing firebase configuration to env variables

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -1,8 +1,15 @@
 import firebase from "firebase/app";
-import config from "@/firebaseConfig";
+//import config from "@/firebaseConfig";
 import "firebase/firestore";
 
-firebase.initializeApp(config);
+firebase.initializeApp({
+  apiKey: process.env.VUE_APP_apiKey,
+  authDomain: process.env.VUE_APP_authDomain,
+  databaseURL: process.env.VUE_APP_databaseURL,
+  projectId: process.env.VUE_APP_projectId,
+  storageBucket: process.env.VUE_APP_storageBucket,
+  messagingSenderId: process.env.VUE_APP_messagingSenderId
+});
 
 // Initialize Cloud Firestore through Firebase
 let db = firebase.firestore();
