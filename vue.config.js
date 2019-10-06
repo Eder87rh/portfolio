6aa111ced6358063d057455b97c9da79ea5d9c28
Eder Ramírez Hernández <eder.ramirez87@gmail.com>
2019-10-06 16:45:58 -0500

    Routes code splitting

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,3 @@
-/* const CompressionWebpackPlugin = require("compression-webpack-plugin");
-var config = require("./webpackConfig"); */
-
 module.exports = {
   css: {
     loaderOptions: {
@@ -9,28 +6,5 @@ module.exports = {
       }
     }
   }
-  /* pluginOptions: {
-    webpackBundleAnalyzer: {
-      openAnalyzer: true
-    }
-  },
-  configureWebpack: {
-    resolve: {
-      alias: {
-        vue$: "vue/dist/vue.esm.js" // 'vue/dist/vue.common.js' for webpack 1
-      }
-    },
-    plugins: [
-      new CompressionWebpackPlugin({
-        filename: "[path].gz[query]",
-        algorithm: "gzip",
-        test: new RegExp(
-          "\\.(" + config.build.productionGzipExtensions.join("|") + ")$"
-        ),
-        threshold: 10240,
-        minRatio: 0.8
-      })
-    ]
-  } */
   //publicPath: process.env.NODE_ENV === "production" ? "/portfolio" : "/"
 };
