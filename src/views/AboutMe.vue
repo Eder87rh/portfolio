943d549e1ff90dd0e6b129521ef0b2b1184664fd
Eder Ramirez <eder.ramirez87@gmail.com>
2022-08-03 15:46:47 -0500

    about me to set react as main skill

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -1,14 +1,10 @@
 <template>
   <div class="about-me">
-    <img
-      class="about-me__vue-logo"
-      src="../../public/img/vue-header.png"
-      alt=""
-    />
+    <img class="about-me__vue-logo" src="../../public/img/react.png" alt="" />
     <div class="about-me__description">
       <p>
         I am an experienced developer located in Mexico who has focused on
-        front-end with Vue.js. Along my career I have developed several
+        front-end with React.js. Throughout my career I have developed several
         projects, always taking care of user experience and quality code.
       </p>
       <button
